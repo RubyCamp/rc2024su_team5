328c224b4f11243fe5f6490ff91bcfbefe1a7f88
cashiopea2019 <cashiopea2019@gmail.com>
2024-08-27 17:07:47 +0900

    マウスの座標

--- user.rb
+++ user.rb
@@ -4,7 +4,12 @@ class User < Player
   def initialize
     super
   end
-
+  def mouse_over_card?(index)
+    mouse_x.between?(50 + index * 150, 50 + index * 150 + @player_images[index].width) &&
+    mouse_y.between?(200, 200 + @player_images[index].height)
+  end
+  
   def trash_card
+   
   end
 end
