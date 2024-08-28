57b8fafc11dbbc0c790983e425c6be72941bc056
GaTo-Rfc <109578116+GaTo-Rfc@users.noreply.github.com>
2024-08-28 14:52:06 +0900

    デッキをシャッフルして手札に配るように変更、手札が画面に表示されるように変更

--- rubycamp2024summer_sample/scenes/game/player.rb
+++ rubycamp2024summer_sample/scenes/game/player.rb
@@ -16,12 +16,12 @@ class Player
 
   # 手札を初期化
   def initialize_hand(referee)
-    HAND_LIMIT.times { @hands << referee.draw_from_deck(self) }
+    # HAND_LIMIT.times { @hands << referee.draw_from_deck(self) }
   end
 
   # カードを引く
   def draw_card(referee, draw_times)
-    draw_times.times { @hands << referee.draw_from_deck(self) }
+    # draw_times.times { @hands << referee.draw_from_deck(self) }
   end
 
   protected
