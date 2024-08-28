1cf2da1f4512d14ee6519b054999f7ab02f43410
GaTo-Rfc <109578116+GaTo-Rfc@users.noreply.github.com>
2024-08-28 09:10:57 +0900

    RoleJudgeクラスのスケルトンを追加

---
+++ role_judge.rb
@@ -0,0 +1,36 @@
+class RoleJudge
+  def initialize
+  end
+
+  def judge
+  end
+
+  private
+
+  def royal_straight_flush?
+  end
+
+  def straight_flush?
+  end
+
+  def four_card?
+  end
+
+  def full_house?
+  end
+
+  def flush?
+  end
+
+  def straight?
+  end
+
+  def three_card?
+  end
+
+  def two_pair?
+  end
+
+  def one_pair?
+  end
+end
